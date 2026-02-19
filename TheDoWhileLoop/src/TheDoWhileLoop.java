5737e63727883454c64643a4f28580640e0ec062
Test Git <dhomseaditya@gmail.com>
2026-02-19 12:49:24 +0530

    Added Do While Loop Program

---
+++ TheDoWhileLoop/src/TheDoWhileLoop.java
@@ -0,0 +1,10 @@
+public class TheDoWhileLoop {
+    public static void main(String[] args) throws Exception {
+        int i = 1;
+
+        do { 
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
