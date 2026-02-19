42d3aded480258f685b56d5a3220d14d30f39068
Test Git <dhomseaditya@gmail.com>
2026-02-19 12:42:19 +0530

    Added While Loop Program

---
+++ TheWhileLoop/src/TheWhileLoop.java
@@ -0,0 +1,10 @@
+public class TheWhileLoop {
+    public static void main(String[] args) throws Exception {
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(i);     
+            i++;       
+        }
+    }
+}
