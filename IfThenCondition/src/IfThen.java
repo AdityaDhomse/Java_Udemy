3f9b438577386d80930d4453fc2f17088ff601b9
Test Git <dhomseaditya@gmail.com>
2026-02-19 11:02:40 +0530

    Added if then program

---
+++ IfThenCondition/src/IfThen.java
@@ -0,0 +1,14 @@
+    public class IfThen {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+        
+        if (isAlien == true)
+            System.out.println("There is no alien.");
+    
+        int topScore = 100;
+        if(topScore == 100)
+            System.out.println("You scored full marks.");
+
+        
+    }
+}
