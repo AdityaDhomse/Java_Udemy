7e896ce260208fe0eabffa2f589b568b5ea33def
Test Git <dhomseaditya@gmail.com>
2026-02-19 11:05:36 +0530

    Added Switch Case Program

---
+++ SwitchCase/src/SwitchCase.java
@@ -0,0 +1,21 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        int switchValue = 3;
+
+        switch (switchValue) {
+            case 1 -> { 
+                System.out.println("Value was 1");
+                break;
+            }
+            case 2 -> {
+                System.err.println("Value was 2");
+                break;
+            }
+            default -> {    
+                System.out.println("Value was not 1 or 2");
+                break;
+            }
+        }
+    }
+}
